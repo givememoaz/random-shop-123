8947fd01ebddcd46b1299b252df8b59d062b76f0
givememoaz <givememoaz00@gmail.com>
2026-01-05 12:37:31 -0600

    Refactor contact function and add panel controls

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function contact(itemName) {
-  alert(
-    "Interested in: " + itemName + "\n\n" +
-    "DM us on Instagram or TikTok to purchase.\n" +
-    "Link in the header!"
-  );
+const panel = document.getElementById("panel");
+
+function openPanel(num) {
+  panel.style.display = "flex";
 }
+
+function closePanel() {
+  panel.style.display = "none";
+}
+
+/* PARALLAX EFFECT */
+window.addEventListener("scroll", () => {
+  document.body.style.backgroundPositionY =
+    window.scrollY * 0.3 + "px";
+});
